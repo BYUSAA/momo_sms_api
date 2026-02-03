69e3f8b199dbcba44e0b42441914139dd924c587
BYUUSAA <byuussaa@gmail.com>
2026-02-03 12:13:32 +0000

    Update performance.py

--- dsa/performance.py
+++ dsa/performance.py
@@ -1,1 +1,20 @@
-print("Performance placeholder")
+import time
+import json
+from dsa.search import linear_search, dictionary_lookup
+
+with open("data/transactions.json") as f:
+    transactions = json.load(f)
+
+transaction_dict = {tx["id"]: tx for tx in transactions}
+target_id = transactions[-1]["id"]
+
+start = time.time()
+linear_search(transactions, target_id)
+linear_time = time.time() - start
+
+start = time.time()
+dictionary_lookup(transaction_dict, target_id)
+dict_time = time.time() - start
+
+print("Linear Search Time:", linear_time)
+print("Dictionary Lookup Time:", dict_time)
