7618741a46925646795c677b660137a2deb1711a
BYUUSAA <byuussaa@gmail.com>
2026-02-03 12:13:08 +0000

    Update search.py

--- dsa/search.py
+++ dsa/search.py
@@ -1,1 +1,9 @@
-print("Search placeholder")
+def linear_search(transactions, transaction_id):
+    for tx in transactions:
+        if tx["id"] == transaction_id:
+            return tx
+    return None
+
+
+def dictionary_lookup(transaction_dict, transaction_id):
+    return transaction_dict.get(transaction_id)
